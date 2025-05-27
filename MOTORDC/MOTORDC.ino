0eeee23de11c7fd68f73fe601cc39c4a373d13d2
JOSUECALDE9876 <josuecalderonruf5@gmail.com>
2025-05-27 09:58:42 -0500

    Update MOTORDC.ino

--- MOTORDC/MOTORDC.ino
+++ MOTORDC/MOTORDC.ino
@@ -6,13 +6,18 @@ void setup() {
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
   pinMode(ENA, OUTPUT);
+}
+
+void loop() {
+  // Giro hacia la derecha
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  analogWrite(ENA, 200);
+  delay(5000);  // Mantiene el giro 5 segundos
 
   // Giro hacia la izquierda
   digitalWrite(IN1, LOW);
   digitalWrite(IN2, HIGH);
-  analogWrite(ENA, 200); // Ajusta la velocidad (0 a 255)
-}
-
-void loop() {
-  // Motor girando continuamente a la izquierda
+  analogWrite(ENA, 200);
+  delay(5000);  // Mantiene el giro 5 segundos
 }
