b70b3cde184581b887f5bc96c06da69110c62fc3
JOSUECALDE9876 <josuecalderonruf5@gmail.com>
2025-06-02 08:47:59 -0500

    Update MOTORDC.ino

--- MOTORDC/MOTORDC.ino
+++ MOTORDC/MOTORDC.ino
@@ -1,12 +1,9 @@
-// Giro alternado (derecha e izquierda)
+// Giro a la derecha (sentido horario)
 #define STEPPER_PIN_1 9
 #define STEPPER_PIN_2 10
 #define STEPPER_PIN_3 11
 #define STEPPER_PIN_4 12
 int step_number = 0;
-bool direction = true;  // true=derecha, false=izquierda
-unsigned long lastChange = 0;
-const long interval = 2000;  // 2 segundos entre cambios
 
 void setup() {
   pinMode(STEPPER_PIN_1, OUTPUT);
@@ -16,21 +13,12 @@ void setup() {
 }
 
 void loop() {
-  unsigned long currentMillis = millis();
-  
-  // Cambia dirección cada 2 segundos
-  if(currentMillis - lastChange >= interval) {
-    lastChange = currentMillis;
-    direction = !direction;  // Invierte la dirección
-  }
-  
-  OneStep(direction);
+  OneStep(true);  // true = derecha (horario)
   delay(2);
 }
 
 void OneStep(bool dir){
   if(dir){
-    // Secuencia derecha (horario)
     switch(step_number){
       case 0:
         digitalWrite(STEPPER_PIN_1, HIGH);
@@ -56,35 +44,7 @@ void OneStep(bool dir){
         digitalWrite(STEPPER_PIN_3, LOW);
         digitalWrite(STEPPER_PIN_4, HIGH);
         break;
-    }
-  } else {
-    // Secuencia izquierda (antihorario)
-    switch(step_number){
-      case 0:
-        digitalWrite(STEPPER_PIN_1, LOW);
-        digitalWrite(STEPPER_PIN_2, LOW);
-        digitalWrite(STEPPER_PIN_3, LOW);
-        digitalWrite(STEPPER_PIN_4, HIGH);
-        break;
-      case 1:
-        digitalWrite(STEPPER_PIN_1, LOW);
-        digitalWrite(STEPPER_PIN_2, LOW);
-        digitalWrite(STEPPER_PIN_3, HIGH);
-        digitalWrite(STEPPER_PIN_4, LOW);
-        break;
-      case 2:
-        digitalWrite(STEPPER_PIN_1, LOW);
-        digitalWrite(STEPPER_PIN_2, HIGH);
-        digitalWrite(STEPPER_PIN_3, LOW);
-        digitalWrite(STEPPER_PIN_4, LOW);
-        break;
-      case 3:
-        digitalWrite(STEPPER_PIN_1, HIGH);
-        digitalWrite(STEPPER_PIN_2, LOW);
-        digitalWrite(STEPPER_PIN_3, LOW);
-        digitalWrite(STEPPER_PIN_4, LOW);
-        break;
-    }
+    } 
   }
   step_number++;
   if(step_number > 3){
