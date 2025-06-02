2c9f01d606a3f1f08453baf0b2fb70d9e4f9d387
JOSUECALDE9876 <josuecalderonruf5@gmail.com>
2025-06-02 09:23:35 -0500

    Update MOTORDC.ino

--- MOTORDC/MOTORDC.ino
+++ MOTORDC/MOTORDC.ino
@@ -1,9 +1,12 @@
-// Giro a la izquierda (sentido antihorario)
+// Giro alternado (derecha e izquierda)
 #define STEPPER_PIN_1 9
 #define STEPPER_PIN_2 10
 #define STEPPER_PIN_3 11
 #define STEPPER_PIN_4 12
 int step_number = 0;
+bool direction = true;  // true=derecha, false=izquierda
+unsigned long lastChange = 0;
+const long interval = 2000;  // 2 segundos entre cambios
 
 void setup() {
   pinMode(STEPPER_PIN_1, OUTPUT);
@@ -13,12 +16,49 @@ void setup() {
 }
 
 void loop() {
-  OneStep(false);  // false = izquierda (antihorario)
+  unsigned long currentMillis = millis();
+  
+  // Cambia dirección cada 2 segundos
+  if(currentMillis - lastChange >= interval) {
+    lastChange = currentMillis;
+    direction = !direction;  // Invierte la dirección
+  }
+  
+  OneStep(direction);
   delay(2);
 }
 
 void OneStep(bool dir){
-  if(!dir){
+  if(dir){
+    // Secuencia derecha (horario)
+    switch(step_number){
+      case 0:
+        digitalWrite(STEPPER_PIN_1, HIGH);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 1:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, HIGH);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 2:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, HIGH);
+        digitalWrite(STEPPER_PIN_4, LOW);
+        break;
+      case 3:
+        digitalWrite(STEPPER_PIN_1, LOW);
+        digitalWrite(STEPPER_PIN_2, LOW);
+        digitalWrite(STEPPER_PIN_3, LOW);
+        digitalWrite(STEPPER_PIN_4, HIGH);
+        break;
+    }
+  } else {
+    // Secuencia izquierda (antihorario)
     switch(step_number){
       case 0:
         digitalWrite(STEPPER_PIN_1, LOW);
